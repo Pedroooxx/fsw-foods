ce7bec8afb0a03e467d02ce679f86fca724bcf11
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-05-01 16:27:53 -0300

    chore: add husky and lintstaged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+const name = "";
+
+console.log(name)
+
 const Home = () => {
 
   return (
-    <div className="bg-red-500 py-5"></div>
+    <div className="bg-red-500 py-5">
+            <h1>hello</h1>
+    </div>
   );
 }
 
