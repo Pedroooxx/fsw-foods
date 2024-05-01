8ad0ec89485da1d65ada4347146d75a18ba4f816
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-05-01 16:33:10 -0300

    chore: add git commit message linter

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-const name = "";
-
-console.log(name)
-
 const Home = () => {
 
   return (
-    <div className="bg-red-500 py-5">
-            <h1>hello</h1>
-    </div>
+    <div className="bg-red-500 py-5"></div>
   );
 }
 
