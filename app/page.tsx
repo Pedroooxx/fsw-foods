86b8950eba45ab534d2b4b036e1e16103a93ee7a
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-05-01 18:30:29 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import Image from "next/image";
 
 const Home = () => {
 
@@ -13,6 +14,17 @@ const Home = () => {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      
+      <div className="px-5 pt-6">
+        <Image src="/promo-banner-01.png"
+          alt="Até 30% de desconto em Pizzas"
+          height={0}
+          width={0}
+          className="w-full h-auto object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
