0c4bbd35b97452ded219ef186c2c202d9f747401
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-05-01 21:46:21 -0300

    feat: and restaurant list component and changing product-list directory name

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
-import ProductsList from "./_components/products-list";
+import ProductsList from "./_components/product-list";
 import Search from "./_components/search";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 
 const Home = async () => {
@@ -54,6 +55,17 @@ const Home = async () => {
 
       <div className="px-5 pt-6">
         <PromoBanner src="/promo-banner-02.png" alt="a partir de R$17,90 em Lanches!"/>
+      </div>
+
+      <div className="py-6 space-y-4">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button variant="ghost" className="h-fit p-0 text-primary hover:bg-transparent">
+            Ver Todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
       </div>
       
     </>
